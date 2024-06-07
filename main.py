814fad362ea4cb2f7a65555fc1b656839efd06c6
Asishkumar <gouda0682@gmail.com>
2024-06-07 09:12:00 +0530

    updated

--- main.py
+++ main.py
@@ -2,36 +2,39 @@ import os
 import random
 import datetime
 
-def makeCommits(start_date: str, end_date: str):
-    start = datetime.datetime.strptime(start_date, '%Y-%m-%d')
-    end = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+def makeCommit(commit_date: str):
+    commit_date_obj = datetime.datetime.strptime(commit_date, '%Y-%m-%d')
+    commit_date_str = commit_date_obj.strftime('%Y-%m-%d')
     
-    current_date = start
-    while current_date <= end:
-        commit_date_str = current_date.strftime('%Y-%m-%d')
-        
-        # Decide the number of commits for this day (random between 1 and 10)
-        num_commits = random.randint(1, 10)
-        
-        # Write commit messages to file
-        with open('data.txt', 'a') as file:
-            for _ in range(num_commits):
-                file.write(f'{commit_date_str} <- this was the commit for the day!!\n')
-        
-        # Staging 
-        os.system('git add data.txt')
+    # Ensure more than 7 commits for this day (random between 8 and 15)
+    num_commits = random.randint(8, 15)
+    
+    # Write commit messages to file
+    with open('data.txt', 'a') as file:
+        for _ in range(num_commits):
+            file.write(f'{commit_date_str} <- this was the commit for the day!!\n')
+    
+    # Staging 
+    os.system('git add data.txt')
 
-        # Commit 
+    # Commit
+    for _ in range(num_commits):
         os.system(f'git commit --date="{commit_date_str}" -m "Commit for {commit_date_str}"')
 
-        # Move to the next day
-        current_date += datetime.timedelta(days=1)
-
-    # After committing for all days, push to the repository
+    # Push to the repository
     os.system('git push')
 
-# Define the range of dates (Jan 1, 2024 to Mar 31, 2024)
-start_date = '2024-01-01'
-end_date = '2024-03-31'
+# Define the start and end date for the commits
+start_date = '2024-06-01'
+end_date = '2024-06-07'
 
-makeCommits(start_date, end_date)
+# Convert string dates to datetime objects
+start_date_obj = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+end_date_obj = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+
+# Generate a list of dates from start_date to end_date
+current_date_obj = start_date_obj
+while current_date_obj <= end_date_obj:
+    commit_date = current_date_obj.strftime('%Y-%m-%d')
+    makeCommit(commit_date)
+    current_date_obj += datetime.timedelta(days=1)
